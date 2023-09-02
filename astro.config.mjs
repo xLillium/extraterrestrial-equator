ba761bbb997a288f6abcc2b5577b9f450496370f
xLillium <nicolas.motillon@gmail.com>
2023-09-03 01:25:34 +0200

    add netlify dependency for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: "server",
+  adapter: netlify()
 });
