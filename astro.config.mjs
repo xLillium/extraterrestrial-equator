6db4788c9d9965799bb72b08d16eb9db05bcb883
xLillium <nicolas.motillon@gmail.com>
2023-09-04 18:07:47 +0200

    Add solidjs and react support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import netlify from "@astrojs/netlify/functions";
+
+import react from "@astrojs/react";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react(), solidJs()],
   output: "server",
   adapter: netlify()
 });
